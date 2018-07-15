bc2767c6b5e5de4a8db2c56344506ec4239b014b
pblquijano <pablo.quijano@dacodes.com.mx>
2018-07-15 13:58:58 -0500

    Launcher Activity Added

---
+++ app/src/main/java/com/pabloquijano/loginpokedex/LauncherActivity.java
@@ -0,0 +1,13 @@
+package com.pabloquijano.loginpokedex;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LauncherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
